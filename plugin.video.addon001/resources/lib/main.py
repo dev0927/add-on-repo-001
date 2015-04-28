95d973eafa12a4d381a96aec183da132bad477fe
dev0927 <dev0927459872549785@gmail.com>
2015-04-27 22:50:20 -0500

    base addon repo files

---
+++ plugin.video.addon001/resources/lib/main.py
@@ -0,0 +1,14 @@
+
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'rtmp://fms.105.net:1935/live playpath=105Test1 live=true'
+li = xbmcgui.ListItem('FM 105!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
