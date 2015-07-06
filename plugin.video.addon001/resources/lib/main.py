e5598d31103cf6e5c282be07b92d788cd7b57f1f
dev0927 <dev0927459872549785@gmail.com>
2015-07-06 14:06:20 -0500

    adding channel

--- plugin.video.addon001/resources/lib/main.py
+++ plugin.video.addon001/resources/lib/main.py
@@ -11,4 +11,9 @@ url = 'rtmp://fms.105.net:1935/live playpath=105Test1 live=true'
 li = xbmcgui.ListItem('FM 105!', iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+url = 'cnRtcDovLzY0LjYyLjE0My41L2xpdmUvZG8lMjBub3QlMjBzdGVhbCUyMG15LVN0cmVhbTI='.decode('base64')
+li = xbmcgui.ListItem('Channel 2', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+
 xbmcplugin.endOfDirectory(addon_handle)
