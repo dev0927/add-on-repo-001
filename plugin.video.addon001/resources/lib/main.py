30f123b2e47ba15d8f633dea40cc2a536c9b8764
dev0927 <dev0927459872549785@gmail.com>
2015-09-17 17:03:33 -0500

    fix

--- plugin.video.addon001/resources/lib/main.py
+++ plugin.video.addon001/resources/lib/main.py
@@ -22,10 +22,10 @@ name='VklFVC1TRVg='
 url = 'cnRtcDovLzY0LjYyLjE0My41L2xpdmUvZG8lMjBub3QlMjBzdGVhbCUyMG15LVN0cmVhbTI='
 config_channel(name, url)
 name='WFhM'
-url = 'aHR0cDovL2ZlLW53LnN2Yy5pcHR2LnJ0LnJ1L2hscy9DSF9DMDNfWFhML3ZhcmlhbnQubTN1OD92ZXJzaW9uPTI='
+url = 'aHR0cDovL2oubXAvWkNIX0MwM19YWExfc2x5'
 config_channel(name, url)
 name='QkxVRSBIVVNUTEVS'
-url = 'aHR0cDovL2ZlLW53LnN2Yy5pcHR2LnJ0LnJ1L2hscy9DSF9DMDNfQkxVRUhVU1RMRVIvdmFyaWFudC5tM3U4P3ZlcnNpb249Mg=='
+url = 'aHR0cDovL2oubXAvWkNIX0MwM19CTFVFSFVTVExFUl9zbHk='
 config_channel(name, url)
 name='TWlhbWkgVFY='
 url = 'aHR0cDovL2s0LnVzYXN0cmVhbXMuY29tOjE5MzUvbWlhbWl0di9zbWlsOm1pYW1pdHYvcGxheWxpc3QubTN1OA=='
